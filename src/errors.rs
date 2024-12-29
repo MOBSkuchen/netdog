c83d8a46e7d1f9e5a0c4bdfce8a91d035c642571
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-12-29 14:45:08 +0100

    Update errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,8 @@
 use std::cmp::PartialEq;
 use std::fmt;
-use crate::errors::HttpError::INTERNAL_ERROR;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum HttpError {
+pub enum HttpCode {
     OK = 200,
     BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
@@ -11,26 +10,33 @@ pub enum HttpError {
     NOT_FOUND = 404,
     METHOD_NOT_ALLOWD = 405,
     INTERNAL_ERROR = 500,
+    NETDOG_ERROR
 }
 
 pub type NtResult<T> = Result<T, NtError>;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct NtError {
-    pub erc: HttpError,
+    pub erc: HttpCode,
     details: String
 }
 
 impl NtError {
-    pub fn new(erc: HttpError, details: Option<String>) -> Self {
+    pub fn new(erc: HttpCode, details: Option<String>) -> Self {
         let details_x = if details.is_some() {details.unwrap()} else {"No details provided".to_string()};
         Self {erc, details: details_x}
+    }
+
+    pub fn to_erf(&self) -> (HttpCode, String) {
+        let mut erc = (self.erc.clone(), self.details.clone());
+        if erc.0 == HttpCode::NETDOG_ERROR {erc.0 = HttpCode::INTERNAL_ERROR}
+        erc
     }
 }
 
 impl fmt::Display for NtError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.erc == INTERNAL_ERROR {
+        if self.erc == HttpCode::NETDOG_ERROR {
             write!(f, "Internal error in NetDog -> {}", self.details).expect("Panicked!");
             return Ok(());
         }
